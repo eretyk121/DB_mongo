9605c9363357e4e61903d3e0785ec3356a73634a
eretyk121 <58330074+eretyk121@users.noreply.github.com>
2020-04-04 14:21:51 +0300

    Update db_mongo.py

--- db_mongo.py
+++ db_mongo.py
@@ -4,7 +4,7 @@ import pymongo
 from pymongo import MongoClient
 client = MongoClient()
 My_db = client['music']
-concerts = My_db['concerts']
+concert = My_db['concert']
 
 
 def read_data(csv_file, db):
@@ -12,8 +12,11 @@ def read_data(csv_file, db):
     with open(csv_file, encoding='utf-8', newline="") as datafile:
         flats_csv = csv.DictReader(datafile, delimiter=",")
         for one in flats_csv:
+            one['Цена'] = int(one['Цена'])
             list_of_data.append(one)
     insert = db.insert_many(list_of_data)
+    for item in db.find():
+        print(item)
 
 
 def find_cheapest(db):
@@ -22,16 +25,13 @@ def find_cheapest(db):
 
 def find_by_name(db, name=input('Введите имя исполнителя: ')):
     pattern = re.compile(f"[а-яА-Яa-zA-z0-9]*{name}[а-яА-Яa-zA-z0-9]*")
-    for item in db.find():
-        text_cor = re.search(pattern, str(item))
-        if text_cor is not None:
-            print(item)
+    result = list(db.find({'Исполнитель': pattern}))
+    print(result)
 
 if __name__ == '__main__':
 
-    find_by_name(concerts)
-    # read_data('artists.csv', concerts)
-    # find_cheapest(artists)
+    find_by_name(concert)
+    # read_data('artists.csv', concert)
+    find_cheapest(concert)
     # for item in concerts.find():
     #     print(item)
-
